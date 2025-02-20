0daf18a34d8bfce6d59f529940d0c9c91fad342b
mahdi hasan <popy2000akter@gmail.com>
2025-02-20 13:05:34 +0500

    added user post and get routes

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//
+//
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qgrba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("task-manager");
+    const userCollection = db.collection("users");
+    const tasksCollection = db.collection("tasks");
+
+    // post a user
+    app.post("/users", async (req, res) => {
+      const articles = req.body;
+      const result = await userCollection.insertOne(articles);
+      res.send(result);
+    });
+
+    // get users data
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Default Route
 app.get("/", (req, res) => {
-    res.send("Server is Running!");
+  res.send("Server is Running!");
 });
 
 // Start Server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
